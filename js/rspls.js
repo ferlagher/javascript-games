1b930cab9bba3cb39a75a9c808fbe6f8797984f9
FerLagHer <flagunahermida@gmail.com>
2022-08-28 00:38:57 -0300

    Responive

--- js/rspls.js
+++ js/rspls.js
@@ -1,34 +1,34 @@
 //Esto es para no tener que comparar todas las jugadas con if/else o switch
 const plays = {
     rock: {
-        scissors: true,
-        paper: false,
-        lizard: true,
-        spock: false,
+        scissors: [true, 'Piedra aplasta tijera'],
+        paper: [false, 'Papel envuelve piedra'],
+        lizard: [true, 'Piedra aplasta lagarto'],
+        spock: [false, 'Spock vaporiza piedra'],
     },
     scissors: {
-        rock: false,
-        paper: true,
-        lizard: true,
-        spock: false,
+        rock: [false, 'Piedra aplasta tijera'],
+        paper: [true, 'Tijera corta papel'],
+        lizard: [true, 'Tijera decapita lagarto'],
+        spock: [false, 'Spock rompe tijera'],
     },
     paper: {
-        rock: true,
-        scissors: false,
-        lizard: false,
-        spock: true,
+        rock: [true, 'Papel envuelve piedra'],
+        scissors: [false, 'Tijera corta papel'],
+        lizard: [false, 'Lagarto devora papel'],
+        spock: [true, 'Papel desautoriza spock'],
     },
     lizard: {
-        rock: false,
-        scissors: false,
-        paper: true,
-        spock: true,
+        rock: [false, 'Piedra aplasta lagarto'],
+        scissors: [false, 'Tijera decapita lagarto'],
+        paper: [true, 'Lagarto devora papel'],
+        spock: [true, 'Lagarto envenena spock'],
     },
     spock: {
-        rock: true,
-        scissors: true,
-        paper: false,
-        lizard: false,
+        rock: [true, 'Spock vaporiza piedra'],
+        scissors: [true, 'Spock rompe tijera'],
+        paper: [false, 'Papel desautoriza spock'],
+        lizard: [false, 'Lagarto envenena spock'],
     },
 }
 
@@ -47,8 +47,13 @@ const output = {
     iaScore: function() {document.querySelector('#iScore').innerHTML = iaScore},
     result: function(res) {document.querySelector('#result').innerHTML = res},
     choices: function() {
-        document.querySelector('#pChoice').setAttribute('xlink:href', `../images/hands.svg#${playerChoice}`)
-        document.querySelector('#iChoice').setAttribute('xlink:href', `../images/hands.svg#${iaChoice}`)
+        document.querySelector('#pChoice').setAttribute('xlink:href', `../images/hands.svg#${playerChoice}`);
+        document.querySelector('#iChoice').setAttribute('xlink:href', `../images/hands.svg#${iaChoice}`);
+        if (playerChoice === iaChoice) {
+            document.querySelector('#vs').innerHTML = 'Empate';
+        } else {
+            document.querySelector('#vs').innerHTML = plays[playerChoice][iaChoice][1];
+        }
     },
 }
 
@@ -73,14 +78,12 @@ const checkWinner = () => {
     if (playerChoice === iaChoice) {
         output.choices();
         output.result('Empate');
-    }
-    else if (plays[playerChoice][iaChoice]) {
+    } else if (plays[playerChoice][iaChoice][0]) {
         output.choices();
         playerScore++;
         output.playerScore();
         output.result('Tú ganas');
-    }
-    else {
+    } else {
         output.choices();
         iaScore++;
         output.iaScore();
