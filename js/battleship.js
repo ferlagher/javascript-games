bb1e5300cc8ce7b8f5e2d1e79863e255d998bf5a
FerLagHer <flagunahermida@gmail.com>
2022-09-12 23:16:24 -0300

    Coords methods

--- js/battleship.js
+++ js/battleship.js
@@ -1,13 +1,16 @@
 const game = {
     boards: document.querySelectorAll('.game__board'),
+    cells: undefined,
 
     createCells () {
         this.boards.forEach(board => {
             for (let i = 0; i < 100; i++) {
                 const div = document.createElement('div');
                 div.classList.add('game__cell');
+                div.dataset.cell = i;
                 board.appendChild(div);
             }
+            this.cells = Array.from(document.querySelectorAll('[data-cell]'))
         })
     }
 }
@@ -20,19 +23,73 @@ class Ship {
         this.size = size;
     }
 
+    
     horizontalCoords(n) {
         const coords = [];
+        const row = m => Math.floor(m / 10);
         for (let i = 0; i < this.size; i++) {
-            coords.push(n + i)
-            return coords
+            const j = i % 2 ? Math.ceil(i / 2) : Math.ceil(-i / 2);
+            let coord = n + j;
+            if (row(n) < row(coord)) {
+                coords.push(coord - this.size);
+            } else if (row(n) > row(coord)) {
+                coords.push(coord + this.size);
+            } else {
+                coords.push(coord);
+            }
         }    
+        return coords
     } 
 
     verticalCoords(n) {
         const coords = [];
         for (let i = 0; i < this.size; i++) {
-            coords.push(n + i * 10)
-            return coords
+            const j = i % 2 ? Math.ceil(i / 2) : Math.ceil(-i / 2);
+            let coord = n + j * 10;
+            if (coord < 0) {
+                coords.push(coord + this.size * 10);
+            } else if (coord < 100) {
+                coords.push(coord);
+            } else {
+                coords.push(coord - this.size * 10);
+            }
         }
+        return coords
     } 
 }
+
+const ships = [
+    new Ship('Portaviones', 'carrier', 5),
+    new Ship('Acorazado', 'battleship', 4),
+    new Ship('Submarino', 'submarine', 3),
+    new Ship('Crucero', 'cruiser', 2),
+    new Ship('Destructor', 'destroyer', 2),
+]
+
+game.createCells();
+
+//Prueba de coordenadas, luego lo borro
+ships[0].verticalCoords(99).forEach(coord => {
+    shipCell = game.cells.find(cell => cell.dataset.cell == coord);
+    shipCell.classList.add('fill')
+});
+
+ships[1].horizontalCoords(50).forEach(coord => {
+    shipCell = game.cells.find(cell => cell.dataset.cell == coord);
+    shipCell.classList.add('fill')
+});
+
+ships[2].horizontalCoords(29).forEach(coord => {
+    shipCell = game.cells.find(cell => cell.dataset.cell == coord);
+    shipCell.classList.add('fill')
+});
+
+ships[3].verticalCoords(0).forEach(coord => {
+    shipCell = game.cells.find(cell => cell.dataset.cell == coord);
+    shipCell.classList.add('fill')
+});
+
+ships[4].verticalCoords(75).forEach(coord => {
+    shipCell = game.cells.find(cell => cell.dataset.cell == coord);
+    shipCell.classList.add('fill')
+});
